99412ee04e2abc36ad48e79497e2d4edaffc0fe3
Anwesa Sinha <110493614+anwesa-sinha@users.noreply.github.com>
2023-06-07 11:39:03 +0530

    Initial state of the news site layout design
    The carousel and the links are working fine.

---
+++ news int/index.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("dash").style.width = "350px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("dash").style.width = "0";
+  }
+
