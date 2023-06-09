9626908bedfd8e55ddb690b32c828868245ada3c
Anwesa Sinha <110493614+anwesa-sinha@users.noreply.github.com>
2023-06-10 01:06:24 +0530

    Delete news int directory

--- news int/index.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("dash").style.width = "350px";
-  }
-  
-  /* Set the width of the sidebar to 0 (hide it) */
-  function closeNav() {
-    document.getElementById("dash").style.width = "0";
-  }
-
